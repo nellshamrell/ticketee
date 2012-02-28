5fbfa802e450ec0e38060be5bb29f38fcea2605b
Nell Shamrell <nshamrell@bluebox.net>
2012-02-28 06:49:56 -0800

    Added remove tag functionality

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,11 @@ Ticketee::Application.routes.draw do
 
   resources :tickets do
     resources :comments
+    resources :tags do
+      member do
+        delete :remove
+      end
+    end
   end
 
   devise_for :users, :controllers => { 
